82b24235ff27e318e949d35cc5ad4cdeae723f97
ROHAN ANAND <anandrohan1705@gmail.com>
2025-10-23 01:27:22 +0530

    add

---
+++ day1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Namaste Duniya" << endl;
+    int a = 123;
+    cout << a << endl;
+    char b = 'v';
+    cout << b << endl;
+    bool bl = true;
+    cout << bl << endl;
+    float f = 12.3;
+    cout << f << endl;
+    double d = 1.23;
+    cout << d << endl;
+    int size = sizeof(a);
+    cout << "Size of a is: " << size << endl;
+}
