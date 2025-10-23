d62bb4c638335c0612d056033589aa59c8c55b9c
ROHAN ANAND <anandrohan1705@gmail.com>
2025-10-23 14:42:03 +0530

    add

---
+++ day6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 24;
+    if (a > 20)
+    {
+        cout << "ROHAN";
+    }
+    else if (a == 24)
+    {
+        cout << "ANAND";
+    }
+    else
+    {
+        cout << "NONE";
+    }
+    cout << a;
+}
