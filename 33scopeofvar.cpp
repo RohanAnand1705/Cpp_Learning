1126fad5f938037a05c676a01510a05fbbb6baaa
ROHAN ANAND <anandrohan1705@gmail.com>
2025-11-09 18:58:29 +0530

    add

---
+++ 33scopeofvar.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 3;
+    cout << a << endl;
+    if (true) {
+        int a = 5; 
+        cout << a << endl; 
+    }
+    int b = 7;
+    cout << b << endl;
+    int i=8;
+    for (int i = 0; i < 8; i++) {
+        cout << "HI" << endl; 
+    }
+    if(1){
+        int b;
+        if(1){
+            int b;
+            if(1){
+                cout<<b<<endl;
+        }
+    }
+}
+}
