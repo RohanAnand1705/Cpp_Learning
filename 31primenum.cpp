d26377edc991f43338453393392e3283d68c8d76
ROHAN ANAND <anandrohan1705@gmail.com>
2025-11-09 17:20:14 +0530

    add

---
+++ 31primenum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(int i=2; i<n; i++){
+        if(n%i==0){
+            cout<<"Not a prime number" << endl;
+            break;}
+        else{
+            cout<<"Prime number" << endl;
+            break;}
+    }
+}
